df054a512cc5848baa53bffd03afe37a27ec203b
Kim T <kimturley@gmail.com>
2024-11-01 22:42:13 -0700

    First api test

--- tests/api.spec.ts
+++ tests/api.spec.ts
@@ -1,12 +1,12 @@
 import { test, expect } from '@playwright/test';
 
 test('Get registry', async ({ request }) => {
-  const issues = await request.get('');
-  expect(issues.ok()).toBeTruthy();
-  expect(await issues.json()).toContainEqual(
-    expect.objectContaining({
-      title: '[Feature] request 1',
-      body: 'Feature description',
-    }),
-  );
+  const endpoint = await request.get('');
+  expect(endpoint.ok()).toBeTruthy();
+  expect(await endpoint.json()).toEqual({
+    name: 'Open Audio Registry',
+    packages: expect.any(Object),
+    url: 'https://open-audio-stack.github.io/open-audio-stack-registry',
+    version: '1.0.0',
+  });
 });
